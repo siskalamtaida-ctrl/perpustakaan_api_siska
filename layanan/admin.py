e6747c15eb87faca9c06bd1137211eed42e24155
Siska <siskalamtiada@gmail.com>
2026-06-30 07:05:59 +0700

    register models to admin

--- layanan/admin.py
+++ layanan/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Kuesioner, JenisPengunjung
 
-# Register your models here.
+admin.site.register(Kuesioner)
+admin.site.register(JenisPengunjung)
